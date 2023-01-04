2b44bab1f3c08102f32a0263130f1efeb4f634d7
Gavin Lee <lee.gavin.k@gmail.com>
2023-01-04 14:07:36 +0100

    text boxes changes

--- src/App.js
+++ src/App.js
@@ -42,16 +42,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          MCC Finder
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
       <div className="add-results-container">
          <form onSubmit={handleSubmit}>
